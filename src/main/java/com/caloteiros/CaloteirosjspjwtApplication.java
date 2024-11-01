4c62560a384169a8545ab9750d40ea990a7b9c5c
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-11-01 18:35:05 -0300

    First commit

---
+++ src/main/java/com/caloteiros/CaloteirosjspjwtApplication.java
@@ -0,0 +1,13 @@
+package com.caloteiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloteirosjspjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloteirosjspjwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caloteiros/CaloteirosjspjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caloteiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloteirosjspjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
