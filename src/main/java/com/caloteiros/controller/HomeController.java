93670617c9af3c103bf4c55b7da006be0fda4670
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-11-01 18:56:40 -0300

    feat: Include maven dependencies, configure view resolver, configure database and a view to Home

---
+++ src/main/java/com/caloteiros/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.caloteiros.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homeView() {
+        return "home";
+    }
+}
